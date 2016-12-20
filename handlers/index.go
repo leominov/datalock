b2394420757b6a0b35ba121a54ea105006b0d0c4
leoderoko <leoderoko@gmail.com>
2016-12-20 23:54:47 +0500

    Add Yandex.Metrika

--- handlers/index.go
+++ handlers/index.go
@@ -34,6 +34,38 @@ const (
         </head>
         <body>
             <iframe src="http://datalock.ru/player/{{.ID}}"></iframe>
+            <!-- Yandex.Metrika counter -->
+            <script type="text/javascript">
+                (function (d, w, c) {
+                    (w[c] = w[c] || []).push(function() {
+                        try {
+                            w.yaCounter41725489 = new Ya.Metrika({
+                                id:41725489,
+                                clickmap:true,
+                                trackLinks:true,
+                                accurateTrackBounce:true
+                            });
+                        } catch(e) { }
+                    });
+
+                    var n = d.getElementsByTagName("script")[0],
+                        s = d.createElement("script"),
+                        f = function () { n.parentNode.insertBefore(s, n); };
+                    s.type = "text/javascript";
+                    s.async = true;
+                    s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+                    if (w.opera == "[object Opera]") {
+                        d.addEventListener("DOMContentLoaded", f, false);
+                    } else { f(); }
+                })(document, window, "yandex_metrika_callbacks");
+            </script>
+            <noscript>
+                <div>
+                    <img src="https://mc.yandex.ru/watch/41725489" style="position:absolute; left:-9999px;" alt="" />
+                </div>
+            </noscript>
+            <!-- /Yandex.Metrika counter -->
         </body>
     </html>
     `
