841541cc5e5ae1e5017a74ad46d2e7102dd95e4e
leoderoko <leoderoko@gmail.com>
2017-05-09 20:27:29 +0500

    Codestyle

--- handlers/javascript.go
+++ handlers/javascript.go
@@ -8,6 +8,13 @@ import (
 	"github.com/leominov/datalock/utils"
 )
 
+var (
+	SwitchHdFix = map[string]string{
+		"swichHDno:": "swichHDdisabled:",
+		"swichHD:":   "swichHDno:",
+	}
+)
+
 func JavaScriptHandler(s *seasonvar.Seasonvar) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		url := s.AbsoluteLink(r.URL.RequestURI())
@@ -17,8 +24,9 @@ func JavaScriptHandler(s *seasonvar.Seasonvar) http.Handler {
 			return
 		}
 		b = strings.Replace(b, seasonvar.Hostname, r.Header.Get("X-HOSTNAME"), -1)
-		b = strings.Replace(b, "swichHDno:", "swichHDdisabled:", -1)
-		b = strings.Replace(b, "swichHD:", "swichHDno:", -1)
+		for old, news := range SwitchHdFix {
+			b = strings.Replace(b, old, news, -1)
+		}
 		w.Write([]byte(b))
 	})
 }
