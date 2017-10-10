c1807dad8d5fff3d478347429cd8927e91b3bd26
Lev Aminov <leoderoko@gmail.com>
2017-10-10 15:35:15 +0500

    Rename local const

--- handlers/middleware.go
+++ handlers/middleware.go
@@ -9,14 +9,14 @@ import (
 )
 
 const (
-	LogginFormat = "%s - \"%s %s %s\" %s\n"
+	logginFormat = "%s - \"%s %s %s\" %s\n"
 )
 
 func MiddlewareHandler(s *server.Server, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		s.FixReferer(r)
 		ip := utils.RealIP(r)
-		log.Printf(LogginFormat, ip, r.Method, r.URL.Path, r.Proto, r.UserAgent())
+		log.Printf(logginFormat, ip, r.Method, r.URL.Path, r.Proto, r.UserAgent())
 		r.Header.Set("User-Agent", utils.RandomUserAgent())
 		h.ServeHTTP(w, r)
 	})
