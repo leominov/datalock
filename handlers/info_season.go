3d9576de53fe61e98eea7c7deffa37f861572fb0
Lev Aminov <leoderoko@gmail.com>
2017-10-11 10:08:44 +0500

    Remove user data from api/info_season

--- handlers/info_season.go
+++ handlers/info_season.go
@@ -5,26 +5,19 @@ import (
 	"net/http"
 
 	"github.com/leominov/datalock/server"
-	"github.com/leominov/datalock/utils"
 )
 
 func InfoSeason(s *server.Server) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		encoder := json.NewEncoder(w)
-		ip := utils.RealIP(r)
-		u := s.GetUser(ip)
 		link := r.URL.Query().Get("url")
 		seasonMeta, err := s.GetCachedSeasonMeta(s.AbsoluteLink(link))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		vars := map[string]interface{}{
-			"user": u,
-			"meta": seasonMeta,
-		}
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Content-Type", "application/json")
-		encoder.Encode(vars)
+		encoder.Encode(seasonMeta)
 	})
 }
