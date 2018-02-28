718e42c9cf8a62c8ea853993b4ac50a0729e74ce
Lev Aminov <leoderoko@gmail.com>
2018-02-28 12:08:30 +0500

    Add ShuffleByInt64

--- utils/shuffle.go
+++ utils/shuffle.go
@@ -5,12 +5,26 @@ import (
 	"reflect"
 )
 
+// Full random shuffle
 func Shuffle(slice interface{}) {
 	rv := reflect.ValueOf(slice)
 	swap := reflect.Swapper(slice)
 	length := rv.Len()
 	for i := length - 1; i > 0; i-- {
 		j := rand.Intn(i + 1)
+		swap(i, j)
+	}
+}
+
+// Controlled shuffle by int64 value
+func ShuffleByInt64(slice interface{}, rndInt int64) {
+	rv := reflect.ValueOf(slice)
+	swap := reflect.Swapper(slice)
+	source := rand.NewSource(rndInt)
+	random := rand.New(source)
+	length := rv.Len()
+	for i := length - 1; i > 0; i-- {
+		j := random.Intn(i + 1)
 		swap(i, j)
 	}
 }
