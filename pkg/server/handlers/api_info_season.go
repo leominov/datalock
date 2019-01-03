e600d9609b3cd3e7edf8df38798786ee55be4566
l.aminov <l.aminov@tinkoff.ru>
2019-01-03 19:20:29 +0500

    Fix code style

--- pkg/server/handlers/api_info_season.go
+++ pkg/server/handlers/api_info_season.go
@@ -10,6 +10,10 @@ import (
 func ApiInfoSeasonHandler(s *server.Server) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		link := r.URL.Query().Get("url")
+		if len(link) == 0 {
+			http.Error(w, "Incorrect request", http.StatusBadRequest)
+			return
+		}
 		link = s.SwitchSeriesLink(link, true)
 		seasonMeta, hitCache, err := s.GetCachedSeasonMeta(s.AbsoluteLink(link))
 		if err != nil {
